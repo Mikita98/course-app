824f0068b074435a06a0451ef2946ba1915b26e7
Mikita Bobryk <mikita.bobryk@medicalib.fr>
2025-05-04 09:18:38 +0200

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import * as path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [UnoCSS(), react()],
-  base: 'https://mikita98.github.io/course-app',
+  base: '/course-app/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
