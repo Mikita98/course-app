729b6e280f944413cb0ad1eb4982d2bf7f44ee86
Mikita Bobryk <mikita.bobryk@medicalib.fr>
2025-05-04 09:18:53 +0200

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import UnoCSS from 'unocss/vite'
 import * as path from 'path'
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [UnoCSS(), react()],
   base: '/course-app/',
